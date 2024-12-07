03ef15704c49a8e3d369d8ee5bbef7129e3ac56c
CovOwe04 <owenacov@gmail.com>
2024-12-07 17:46:16 -0500

    Added Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import '../styles/main.css';
+
+export default function Header () {
+    return (
+        <div className="HeaderContainer">
+            <h1>Bog Logs</h1>
+        </div>
+    );
+  }
