d5d98b1e9e37563496f7a66031c0668c73855d2e
covjus01 <covjus01@gmail.com>
2024-12-31 16:44:39 -0500

    Atomized post and reply components

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-export default function Post() {
-    const [post, setPost] = useState({title: "", description: ""});
-
-
+export default function Reply() {
+    const [reply, setReply] = useState({title: "", description: "", isParent: false});
     //Sends a Post request with the post content on the server
     const handleSubmit = () => {
-        fetch("localhost:5000/post", {
+        fetch("localhost:5000/reply", {
         method: "POST",
         headers: {
             "Content-Type": "application-json",
             },
-        body: JSON.stringify(post),
+        body: JSON.stringify(reply),
         })
         .then((response) => {
             if(!response.ok) {
@@ -27,11 +25,10 @@ export default function Post() {
     }
     return (
         <div className="PostContainer">
-            <input className="PostTitle" />  <br />
             <textarea className="PostInput" /> <br />
             <div className="Flex">
                 <div className="FillerDiv"/>
-                <button className="PostBtn">Post</button>
+                {<button className="PostBtn">Reply</button>}
             </div>
         </div>
     );
